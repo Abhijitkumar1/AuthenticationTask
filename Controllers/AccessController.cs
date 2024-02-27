ff5a093e75768cdc600d9b66989f805104d72153
kumar <kumar@DESKTOP-LQIOUVB>
2024-02-27 10:30:12 +0530

    Add project files.

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,48 @@
+﻿using AuthenticationTask.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AuthenticationTask.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimsUser = HttpContext.User;
+            if (claimsUser.Identity.IsAuthenticated) 
+                return RedirectToAction("Student", "Home");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(Login modelLogin)
+        {
+            if (modelLogin.Email == "kumarabhijit018@gmail.com" &&
+                modelLogin.Password == "12345"
+               )
+            {
+                List<Claim> claims = new List<Claim>();
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email);
+                    new Claim("otherProperties", "Example Role");
+                };
+                ClaimsIdentity identity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = false,
+                };
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity), properties);
+                return RedirectToAction("Student", "Home");
+            }
+            ViewData["ValidateMessage"] = "User Not Found";
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using AuthenticationTask.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+
+namespace AuthenticationTask.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        private readonly StudentContext _context;
+
+        public HomeController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Privacy()
+        {
+            return View();
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Access");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Student()
+        {
+            var ShowData = _context.Students.ToList();
+            return View(ShowData);
+            
+        }
+        public IActionResult Admin()
+        {
+            return View();
+
+        }
+        public IActionResult GetData()
+        {
+            var ShowData = _context.Students.ToList();
+            return Json(ShowData);
+        }
+        public IActionResult Create(Student model)
+        {
+
+            var blog = new Student()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Department = model.Department,
+                Email = model.Email,
+
+            };
+            _context.Students.Add(blog);
+            _context.SaveChanges();
+            return Json(blog);
+
+
+        }
+        public IActionResult UpdateData(Student app)
+        {
+            var oldData = _context.Students.Find(app.Id);
+
+            if (oldData != null)
+            {
+
+                oldData.Name = app.Name;
+                oldData.Department = app.Department;
+                oldData.Email = app.Email;
+                _context.Students.Update(oldData);
+                _context.SaveChanges();
+                return new JsonResult("OK");
+            }
+            return new JsonResult("Not Found");
+
+        }
+        public IActionResult DeleteData(int Id)
+        {
+            var Data = _context.Students.Find(Id);
+
+            _context.Students.Remove(Data);
+            _context.SaveChanges();
+            return new JsonResult("OK");
+        }
+    }
+}
